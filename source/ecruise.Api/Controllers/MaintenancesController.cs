56c72a1fb06eab41204c4781e922ba416f2e1af6
Lyrex <admin@lyrex.net>
2017-06-01 18:55:25 +0200

    Implement database interaction in Maintenances

--- source/ecruise.Api/Controllers/MaintenancesController.cs
+++ source/ecruise.Api/Controllers/MaintenancesController.cs
@@ -1,56 +1,66 @@
-using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 using ecruise.Models;
+using DbMaintenance = ecruise.Database.Models.Maintenance;
 
 namespace ecruise.Api.Controllers
 {
     public class MaintenancesController : BaseController
     {
         // GET: /Maintenances
-        [HttpGet(Name="GetAllMaintenances")]
+        [HttpGet(Name = "GetAllMaintenances")]
         public IActionResult Get()
         {
-            Maintenance m1 = new Maintenance(1, false, 3000, null);
-            Maintenance m2 = new Maintenance(2, false, null, new DateTime(2017, 5, 8, 13, 37, 0, DateTimeKind.Utc));
-            Maintenance m3 = new Maintenance(3, true, null, null);
+            ImmutableList<DbMaintenance> maintenances = Context.Maintenances.ToImmutableList();
 
-            return Ok(new List<Maintenance> {m1, m2, m3});
+            if (maintenances.Count == 0)
+                return NoContent();
+
+            return Ok(maintenances);
         }
 
         // GET: /Maintenances/5
         [HttpGet("{id}", Name = "GetMaintenance")]
         public IActionResult Get(uint id)
         {
-            if (ModelState.IsValid && id > 0 && id < 3)
-            {
-                Maintenance m = new Maintenance(1, false, 3000, null);
+            if (!ModelState.IsValid)
+                return BadRequest(new Error(400, GetModelStateErrorString(),
+                    "An error occured. Please check the message for further information."));
 
-                return Ok(m);
-            }
-            else if (ModelState.IsValid && (id >= 3 || id == 0))
-            {
-                return NotFound(new Error(1, "Maintenance with requested id does not exist.",
-                    "An error occured. Please check the message for further information."));
-            }
+            DbMaintenance maintenance = Context.Maintenances.Find(id);
+
+            if (maintenance == null)
+                return NotFound(new Error(201, "Maintenance with requested id does not exist.",
+                    $"There is no maintenance that has the id {id}."));
             else
-            {
-                return BadRequest(new Error(1, "The id given was not formatted correctly. Id has to be unsinged int",
-                    "An error occured. Please check the message for further information."));
-            }
+                return Ok(maintenance);
         }
-        
+
         // POST: /Maintenances
-        [HttpPost(Name="CreateMaintenance")]
-        public IActionResult Post([FromBody]Maintenance m)
+        [HttpPost(Name = "CreateMaintenance")]
+        public IActionResult Post([FromBody] Maintenance m)
         {
-            if (ModelState.IsValid)
-                return Created($"{BasePath}/maintenances/1",
-                    new PostReference(1, $"{BasePath}/maintenances/1"));
-            else
-                return BadRequest(new Error(1, GetModelStateErrorString(),
+            if (!ModelState.IsValid)
+                return BadRequest(new Error(400, GetModelStateErrorString(),
                     "An error occured. Please check the message for further information."));
+
+            DbMaintenance insertMaintenance =
+                new DbMaintenance
+                {
+                    Spontaneously = m.Spontaneously,
+                    AtMileage = m.AtMileage,
+                    AtDate = m.AtDate
+                };
+
+
+            var insert = Context.Maintenances.Add(insertMaintenance);
+            Context.SaveChanges();
+
+            return Created($"{BasePath}/maintenances/{insert.Entity.MaintenanceId}",
+                new PostReference(insert.Entity.MaintenanceId, $"{BasePath}/maintenances/{insert.Entity.MaintenanceId}"));
         }
     }
 }
