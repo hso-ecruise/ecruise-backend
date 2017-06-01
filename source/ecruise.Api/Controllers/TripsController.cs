dcfb6bbed16b3a33f88e6be5721ae63bd0510180
Lyrex <admin@lyrex.net>
2017-06-01 19:15:33 +0200

    Implement database interaction in Trips

--- source/ecruise.Api/Controllers/TripsController.cs
+++ source/ecruise.Api/Controllers/TripsController.cs
@@ -1,7 +1,9 @@
-using System;
-using System.Collections.Generic;
-using ecruise.Models;
+using System.Collections.Immutable;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+
+using ecruise.Models;
+using DbTrip = ecruise.Database.Models.Trip;
 
 namespace ecruise.Api.Controllers
 {
@@ -11,92 +13,104 @@ namespace ecruise.Api.Controllers
         [HttpGet(Name = "GetAllTrips")]
         public IActionResult GetAll()
         {
-            Trip t1 = new Trip(1, null, 3, null, null, null, null, null);
-            Trip t2 = new Trip(2, 6, 3, new DateTime(2017, 5, 8, 13, 37, 0, DateTimeKind.Utc), null, 18, null, null);
-            Trip t3 = new Trip(3, 6, 3, new DateTime(2017, 5, 8, 13, 37, 0, DateTimeKind.Utc),
-                new DateTime(2017, 5, 8, 14, 42, 0, DateTimeKind.Utc), 18, 7, 7412);
+            ImmutableList<DbTrip> trips = Context.Trips.ToImmutableList();
+            if (trips.Count == 0)
+                return NoContent();
 
-            return Ok(new List<Trip> {t1, t2, t3});
+            return Ok(trips);
         }
 
         // POST: /trips
         [HttpPost(Name = "CreateTrip")]
         public IActionResult Post([FromBody] Trip trip)
         {
-            if (ModelState.IsValid)
-                return Created($"{BasePath}/trips/1",
-                    new PostReference(trip.TripId, $"{BasePath}/trips/1"));
-            else
-                return BadRequest(new Error(1, GetModelStateErrorString(),
+            if (!ModelState.IsValid)
+                return BadRequest(new Error(400, GetModelStateErrorString(),
                     "An error occured. Please check the message for further information."));
+
+            DbTrip insertTrip = new DbTrip
+            {
+                CarId = trip.CarId,
+                CustomerId = trip.CustomerId,
+                StartDate = trip.StartDate,
+                EndDate = trip.EndDate,
+
+                StartChargingStationId = trip.StartChargingStationId,
+                EndChargingStationId = trip.EndChargingStationId,
+
+                // whole entities (?)
+                Car = Context.Cars.Find(trip.CarId),
+                Customer = Context.Customers.Find(trip.CustomerId),
+
+                StartChargingStation = Context.ChargingStations.Find(trip.StartChargingStationId),
+                EndChargingStation = Context.ChargingStations.Find(trip.EndChargingStationId)
+            };
+
+            var inserted = Context.Trips.Add(insertTrip);
+
+            return Created($"{BasePath}/trips/{inserted.Entity.TripId}",
+                new PostReference(inserted.Entity.TripId, $"{BasePath}/trips/{inserted.Entity.TripId}"));
         }
 
         // GET: /trips/1
         [HttpGet("{id}", Name = "GetTrip")]
-        public IActionResult Get(uint id)
+        public IActionResult GetOne(uint id)
         {
-            if (ModelState.IsValid && id < 3)
-            {
-                Trip trip = new Trip(id, 6, 3, new DateTime(2017, 5, 8, 13, 37, 0, DateTimeKind.Utc),
-                    new DateTime(2017, 5, 8, 14, 42, 0, DateTimeKind.Utc), 18, 7, 2594);
+            if (!ModelState.IsValid)
+                return BadRequest(new Error(400, GetModelStateErrorString(),
+                    "An error occured. Please check the message for further information."));
 
-                return Ok(trip);
-            }
-            else if (ModelState.IsValid && (id >= 3 || id == 0))
-            {
-                return NotFound(new Error(1, "Trip with requested trip id does not exist.",
-                    "An error occured. Please check the message for further information."));
-            }
-            else
-            {
-                return BadRequest(new Error(1, "The id given was not formatted correctly. Id has to be unsinged int",
-                    "An error occured. Please check the message for further information."));
-            }
+            DbTrip trip = Context.Trips.Find(id);
+            if (trip == null)
+                return NotFound(new Error(201, "Trip with requested id does not exist.",
+                    $"There is no trip that has the id {id}."));
+
+            return Ok(trip);
         }
 
         // PATCH: /trips/1
         [HttpPatch("{id}")]
         public IActionResult Patch(uint id, [FromBody] TripUpdate trip)
         {
-            if (ModelState.IsValid && id < 3 && id > 0)
-            {
-                return Ok(new PostReference(id, $"{BasePath}/trips/{id}"));
-            }
-            else if (ModelState.IsValid && id >= 3)
-            {
-                return NotFound(new Error(1, "Trip with requested id does not exist.",
+            if (!ModelState.IsValid)
+                return BadRequest(new Error(400, GetModelStateErrorString(),
                     "An error occured. Please check the message for further information."));
-            }
-            else
+
+            DbTrip dbtrip = Context.Trips.Find(id);
+            if (dbtrip == null)
+                return NotFound(new Error(201, "Trip with requested id does not exist.",
+                    $"There is no trip that has the id {id}."));
+
+            using (var transaction = Context.Database.BeginTransaction())
             {
-                return BadRequest(new Error(1, GetModelStateErrorString(),
-                    "An error occured. Please check the message for further information."));
+                dbtrip.EndChargingStationId = trip.EndChargingStationId;
+                dbtrip.DistanceTravelled = trip.DistanceTravelled;
+
+                // update entity (?)
+                dbtrip.EndChargingStation = Context.ChargingStations.Find(trip.EndChargingStationId);
+
+                transaction.Commit();
             }
+
+            return Ok(new PostReference(id, $"{BasePath}/trips/{id}"));
         }
 
         // GET: /trips/by-car/5
         [HttpGet("by-car/{carId}", Name = "GetBookingsByCar")]
         public IActionResult GetByCarId(uint carId)
         {
-            if (ModelState.IsValid && carId < 3 && carId > 0)
-            {
-                Trip t1 = new Trip(2, carId, 3, new DateTime(2017, 5, 8, 13, 37, 0, DateTimeKind.Utc),
-                    new DateTime(2017, 5, 8, 14, 42, 0, DateTimeKind.Utc), 18, 7, 2594);
-
-                Trip t2 = new Trip(6, carId, 3, new DateTime(2017, 5, 10, 05, 13, 0, DateTimeKind.Utc),
-                    new DateTime(2017, 5, 10, 09, 47, 0, DateTimeKind.Utc), 12, 4, 16772);
-
-                return Ok(new List<Trip>{t1, t2});
-            }
-            else if (ModelState.IsValid && carId >= 3)
-            {
-                return NotFound("Trip with requested car id does not exist.");
-            }
-            else
-            {
-                return BadRequest(new Error(1, "The id given was not formatted correctly. CarId must be unsinged int",
+            if (!ModelState.IsValid)
+                return BadRequest(new Error(400, GetModelStateErrorString(),
                     "An error occured. Please check the message for further information."));
-            }
+
+            ImmutableList<DbTrip> trips = Context.Trips
+                .Where(t => t.CarId == carId)
+                .ToImmutableList();
+
+            if (trips.Count == 0)
+                return NoContent();
+
+            return Ok(trips);
         }
     }
 }
