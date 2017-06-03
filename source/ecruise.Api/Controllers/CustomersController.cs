1584d4ce5c787de29d2e6275bbb83773d08dba3a
Lyrex <admin@lyrex.net>
2017-06-04 00:48:47 +0200

    Implement user registration and activation
    From now on, user's can register on the homepage and they receive a
     confirmation email on which they have to activate their account.
    The sending of the email now works as well as the user activation link.

--- source/ecruise.Api/Controllers/CustomersController.cs
+++ source/ecruise.Api/Controllers/CustomersController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using ecruise.Database.Models;
 using Microsoft.AspNetCore.Mvc;
 
 using ecruise.Models;
--- source/ecruise.Api/Controllers/PublicController.cs
+++ source/ecruise.Api/Controllers/PublicController.cs
@@ -70,7 +70,7 @@ namespace ecruise.Api.Controllers
         }
 
         // GET: /public/activate/login@ecruise.me/F3E64113EAC3432FFE968942674E98C6B01987F69EADEA90EEA1F8C809AB3DEE
-        [HttpGet("Activate/{email}/{token}", Name = "ActivateAccount")]
+        [HttpGet("activate/{email}/{token}", Name = "ActivateAccount")]
         public IActionResult Activate([FromRoute] string email, [FromRoute] string token)
         {
             if (!ModelState.IsValid)
@@ -80,7 +80,7 @@ namespace ecruise.Api.Controllers
             // find matching customer
             DbCustomer customer = Context.Customers.First(c => c.Email == email);
             if (customer == null)
-                return NotFound();
+                return Redirect("https://ecruise.me/nocustomer");
 
             DbCustomerToken activationToken =
                 Context.CustomerTokens
@@ -91,7 +91,7 @@ namespace ecruise.Api.Controllers
 
             // there is no such activation token
             if (activationToken == null)
-                return NotFound();
+                return Redirect("https://ecruise.me/notoken");
 
             using (var transaction = Context.Database.BeginTransaction())
             {
@@ -106,7 +106,7 @@ namespace ecruise.Api.Controllers
                 Context.SaveChangesAsync();
             }
 
-            return NoContent();
+            return Redirect("https://ecruise.me/start");
         }
 
         // POST: /public/register
@@ -119,12 +119,15 @@ namespace ecruise.Api.Controllers
                 return BadRequest(new Error(401, GetModelStateErrorString(),
                     "An error occured. Please check the message for further information."));
 
+            // create customer password salt
             string passwordSalt = Guid.NewGuid().ToString().Replace("-", string.Empty).Substring(0, 16);
 
+            // create customer password hash
             SHA256 alg = SHA256.Create();
             byte[] result = alg.ComputeHash(Encoding.UTF8.GetBytes(r.Password + passwordSalt));
             string passwordHash = BitConverter.ToString(result).ToLowerInvariant().Replace("-", "");
 
+            // create database user model
             DbCustomer insertCustomer =
                 new DbCustomer
                 {
@@ -142,9 +145,38 @@ namespace ecruise.Api.Controllers
                     AddressExtraLine = r.AddressExtraLine
                 };
 
+            // save customer to database
             var insert = Context.Customers.Add(insertCustomer);
             Context.SaveChanges();
 
+            // generate customer authentification token for email activation
+            var crypt = RandomNumberGenerator.Create();
+            var randBytes = new byte[64];
+            crypt.GetBytes(randBytes);
+
+            string activationToken = BitConverter.ToString(randBytes).ToLowerInvariant().Replace("-", "");
+            crypt.Dispose();
+
+            Context.CustomerTokens.Add(
+                CustomerTokenAssembler.AssembleEntity(0,
+                    new CustomerToken(0, (uint)insert.Entity.CustomerId, CustomerToken.TokenTypeEnum.EmailActivation,
+                        activationToken, DateTime.UtcNow, null)
+                )
+            );
+            var save = Context.SaveChangesAsync();
+
+            CustomerAssembler.AssembleModel(insert.Entity).SendMail("Deine Registrierung bei eCruise", string.Format(@"Hallo {0},<br>
+herzlich willkommen bei eCruise!<br>
+<br>
+Du bist noch nicht ganz fertig mit deiner Registrierung. Um sie abzuschlieﬂen, fehlt nur noch ein kleiner Schritt:<br>
+Mit einem Klick auf den folgenden Link best‰tigst du deine Anmeldung:<br>
+<br>
+<a href=""https://api.ecruise.me/v1/public/activate/{1}/{2}"">https://api.ecruise.me/v1/public/activate/{1}/{2}</a><br>
+<br>
+Wir freuen uns auf dich!<br>
+Dein eCruise-Team", r.FirstName, r.Email, activationToken));
+
+            save.Wait();
             return Created($"{BasePath}/customers/{insert.Entity.CustomerId}",
                 new PostReference((uint)insert.Entity.CustomerId, $"{BasePath}/customers/{insert.Entity.CustomerId}"));
         }
