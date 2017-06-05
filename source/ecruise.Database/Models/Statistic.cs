a88ec8d8d75a47f58bdbc0b592f6e763594e0b65
Felix Fischbach <felixfischbach@gmx.de>
2017-06-05 16:46:35 +0200

    Match statistic entity to model

--- source/ecruise.Database/Models/Statistic.cs
+++ source/ecruise.Database/Models/Statistic.cs
@@ -7,5 +7,7 @@ namespace ecruise.Database.Models
         public DateTime Date { get; set; }
         public uint Bookings { get; set; }
         public double AverageChargeLevel { get; set; }
+        public uint CarsInUse { get; set; }
+        public uint CarsCharging { get; set; }
     }
 }
