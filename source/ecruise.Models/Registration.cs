e67464c1dcad2ad68be84eb23cc9f52349f10f6f
Felix Fischbach <felixfischbach@gmx.de>
2017-06-06 17:56:45 +0200

    Revert "Revert "Add additional data to Registration""
    This reverts commit d5aa9f73d0133373e02ef5352e44df29cd226eaa.

--- source/ecruise.Models/Registration.cs
+++ source/ecruise.Models/Registration.cs
@@ -1,33 +1,15 @@
-using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
-using Newtonsoft.Json;
 
 namespace ecruise.Models
 {
     public class Registration
-        : IEquatable<Registration>
+        : Customer
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Registration" /> class.
-        /// </summary>
-        /// <param name="email">Email (required)</param>
-        /// <param name="password">Password (required)</param>
-        /// <param name="firstName">FirstName (required)</param>
-        /// <param name="lastName">LastName (required)</param>
-        public Registration(string email, string password, string firstName, string lastName)
+        public Registration(uint customerId, string password, string email, string phoneNumber, string chipCardUid, string firstName, string lastName, string country, string city, uint zipCode, string street, string houseNumber, string addressExtraLine, bool activated = false, bool verified = false) 
+            : base(customerId, email, phoneNumber, chipCardUid, firstName, lastName, country, city, zipCode, street, houseNumber, addressExtraLine, activated, verified)
         {
-            Email = email;
             Password = password;
-            FirstName = firstName;
-            LastName = lastName;
         }
-
-        /// <summary>
-        /// Gets Email
-        /// </summary>
-        [Required, StringLength(64), DataType(DataType.EmailAddress)]
-        public string Email { get; }
 
         /// <summary>
         /// Gets Password
@@ -36,105 +18,5 @@ namespace ecruise.Models
         [StringLength(int.MaxValue, MinimumLength = 8,
             ErrorMessage = "The field Password must be a string a minimum length of '8'.")]
         public string Password { get; }
-
-        /// <summary>
-        /// Gets FirstName
-        /// </summary>
-        [Required, StringLength(48)]
-        public string FirstName { get; }
-
-        /// <summary>
-        /// Gets LastName
-        /// </summary>
-        [Required, StringLength(48)]
-        public string LastName { get; }
-
-        /// <summary>
-        /// Returns the string presentation of the object
-        /// </summary>
-        /// <returns>String presentation of the object</returns>
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            sb.Append("class Registration {\n");
-            sb.Append("  Email: ").Append(Email).Append("\n");
-            sb.Append("  Password: ").Append(Password).Append("\n");
-            sb.Append("  FirstName: ").Append(FirstName).Append("\n");
-            sb.Append("  LastName: ").Append(LastName).Append("\n");
-            sb.Append("}\n");
-            return sb.ToString();
-        }
-
-        /// <summary>
-        /// Returns the JSON string presentation of the object
-        /// </summary>
-        /// <returns>JSON string presentation of the object</returns>
-        public string ToJson()
-        {
-            return JsonConvert.SerializeObject(this, Formatting.Indented);
-        }
-
-        /// <summary>
-        /// Returns true if objects are equal
-        /// </summary>
-        /// <param name="obj">Object to be compared</param>
-        /// <returns>Boolean</returns>
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
-            return Equals((Registration)obj);
-        }
-
-        /// <summary>
-        /// Returns true if Registration instances are equal
-        /// </summary>
-        /// <param name="other">Instance of Registration to be compared</param>
-        /// <returns>Boolean</returns>
-        public bool Equals(Registration other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-
-            return
-                (Email == other.Email || Email.Equals(other.Email)) &&
-                (Password == other.Password || Password.Equals(other.Password)) &&
-                (FirstName == other.FirstName || FirstName.Equals(other.FirstName)) &&
-                (LastName == other.LastName || LastName.Equals(other.LastName));
-        }
-
-        /// <summary>
-        /// Gets the hash code
-        /// </summary>
-        /// <returns>Hash code</returns>
-        public override int GetHashCode()
-        {
-            unchecked
-            {
-                int hash = 41;
-
-                hash = hash * 59 + Email.GetHashCode();
-                hash = hash * 59 + Password.GetHashCode();
-                hash = hash * 59 + FirstName.GetHashCode();
-                hash = hash * 59 + LastName.GetHashCode();
-
-                return hash;
-            }
-        }
-
-        #region Operators
-
-        public static bool operator ==(Registration left, Registration right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(Registration left, Registration right)
-        {
-            return !Equals(left, right);
-        }
-
-        #endregion Operators
     }
 }
